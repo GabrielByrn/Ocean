dfdffc8c4f4366ab8d308dcad92339f814e6b3b6
GabrielByrn <sofi20040908@gmail.com>
2022-07-19 10:16:55 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Практика_проект__1.Classes;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ocean myOcean = new Ocean();
+            myOcean.Initialize();
+            myOcean.Run();
+            Console.ReadKey();
+        }
+    }
+}
